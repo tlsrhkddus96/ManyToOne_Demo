111cd9f2015de10edfddd3eaa9325fd1a2635888
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-06-01 18:32:16 +0900

    JPQL-

--- src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -99,6 +99,11 @@ public class BoardRepositoryTests {
         System.out.println(Arrays.toString(arr));
     }
 
+    @Test
+    public void testSearch1(){
+        boardRepository.search1();
+    }
+
 
 }
 
